e9a7da2ed9733c030a24b9cfecaf4ac018a6b038
Afeefnih <afeefz2003@gmail.com>
2024-06-07 22:23:44 +0800

    rates

---
+++ KTMKomuter/Controllers/KtmKomuterController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KTMKomuter.Controllers
+{
+    public class KtmKomuterController : Controller
+    {
+        private readonly IConfiguration configuration;
+        public KtmKomuterController(IConfiguration config)
+        {
+            this.configuration = config;
+        }
+    }
+}
